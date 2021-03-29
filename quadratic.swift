4c90844a77274e0e4ebd01d01e46fa4a0e5dfe9d
Will Gibson <80238097+will-gibson@users.noreply.github.com>
2021-03-29 16:14:00 -0400

    Create quadratic.swift

---
+++ quadratic.swift
@@ -0,0 +1,14 @@
+// Quadratic Formula
+
+var a: Double = 3
+var b: Double = -11
+var c: Double = -4
+
+var root1: Double
+var root2: Double
+
+root1 = (-b + (b*b - 4*a*c).squareRoot()) / (2*a)
+root2 = (-b - (b*b - 4*a*c).squareRoot()) / (2*a)
+
+print(root1)
+print(root2)
